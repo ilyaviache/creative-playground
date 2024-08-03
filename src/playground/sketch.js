5f282c724ac4de3c70bb3e558c9bfa2dcbdb38a3
ilyaviache <ilya.viache@gmail.com>
2024-08-03 21:35:43 +0700

    red dots

--- src/playground/sketch.js
+++ src/playground/sketch.js
@@ -1,13 +1,55 @@
-const canvasSketch = require('canvas-sketch');
+import canvasSketch from 'canvas-sketch';
+import { lerp } from 'canvas-sketch-util/math';
+import random from 'canvas-sketch-util/random';
+import palettes from 'nice-color-palettes';
 
 const settings = {
   dimensions: [ 2048, 2048 ]
 };
 
 const sketch = () => {
+  const createGrid = () => {
+    const points = [];
+    const count = 30;
+    for (let x = 0; x < count; x++) {
+      for (let y = 0; y < count; y++) {
+        const u = count <= 1 ? 0.5 : x / (count - 1);
+        const v = count <= 1 ? 0.5 : y / (count - 1);
+        points.push({
+          color: 'red',
+          radius: Math.abs(0.01 + random.gaussian() * 0.01),
+          position: [ u, v ]
+        });
+      }
+    }
+    return points;
+  };
+
+  random.setSeed(512);
+  const points = createGrid().filter(() => random.value() > 0.5);
+  const margin = 400;
+
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
+
+    points.forEach(data => {
+      const {
+        position,
+        radius,
+        color
+      } = data;
+
+      const [ u, v ] = position;
+
+      const x = lerp(margin, width - margin, u);
+      const y = lerp(margin, height - margin, v);
+
+      context.beginPath();
+      context.arc(x, y, radius * width, 0, Math.PI * 2, false);
+      context.fillStyle = color;
+      context.fill();
+    });
   };
 };
 
